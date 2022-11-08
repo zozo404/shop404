6d20cb65df99e5779259edbe5c00a0f3a6f7a306
zozo404 <enzocuny66@gmail.com>
2022-11-08 12:07:56 +0100

    ✨ | add tailwindcss

--- config/commandes.php
+++ config/commandes.php
@@ -11,6 +11,21 @@ function ajouter($nom,$image,$prix,$desc){
 function afficher(){
     if(require("connexion.php")){
         $req=$access->prepare("SELECT * from produits ORDER BY id DESC");
+        $req->execute();
+        $data=$req->fetchAll(PDO::FETCH_OBJ);
+        return $data;
+        // erreur ?
+        $req->closeCursor();
+
+    }
+}
+
+function supprimer($id){
+    if(require("connexion.php")){
+        // DELETE *
+        $req = $access->prepare("DELETE FROM produits WHERE id=?");
+        $req->execute(array($id));
+
     }
 }
 
