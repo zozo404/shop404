77f61b05b589aa9c4a2ea4f72fa9ca03b05a4f10
zozo404 <enzocuny66@gmail.com>
2022-11-08 18:48:11 +0100

    ✨ |start of cards and add 'alt' for img

--- config/commandes.php
+++ config/commandes.php
@@ -1,8 +1,8 @@
 <?php
 
-function ajouter($nom,$image,$prix,$desc){
+function ajouter($nom,$image,$alt,$prix,$desc){
     if(require("connexion.php")){
-        $req = $access->prepare("INSERT INTO produits (image, nom, prix, description) VALUE ($image,$nom,$prix,$desc)");
+        $req = $access->prepare("INSERT INTO produits (image, alt, nom, prix, description) VALUE ($image,$alt,$nom,$prix,$desc)");
         $req->execute(array($nom,$image,$prix,$desc));
         $req->closeCursor();
     }
