e75cc5bbdb253501e2aee76402030f92ad668414
zozo404 <enzocuny66@gmail.com>
2022-11-08 11:45:52 +0100

    ✨ |add command

--- config/commandes.php
+++ config/commandes.php
@@ -2,7 +2,15 @@
 
 function ajouter($nom,$image,$prix,$desc){
     if(require("connexion.php")){
-        
+        $req = $access->prepare("INSERT INTO produits (image, nom, prix, description) VALUE ($image,$nom,$prix,$desc)");
+        $req->execute(array($nom,$image,$prix,$desc));
+        $req->closeCursor();
+    }
+}
+
+function afficher(){
+    if(require("connexion.php")){
+        $req=$access->prepare("SELECT * from produits ORDER BY id DESC");
     }
 }
 
