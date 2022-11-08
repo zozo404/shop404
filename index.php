55ff980028a405a98b04bdda21f8685e2d9c5b72
zozo404 <enzocuny66@gmail.com>
2022-11-08 14:29:23 +0100

    🔧 |config tailwindcss

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="/dist/output.css" rel="stylesheet">
+    <link href="dist/index.css" rel="stylesheet">
 </head>
 <body>
-    
+    <h1 class="text-center">ff</h1>
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,php}"],
+  content: ["./*.php"],
   theme: {
     extend: {},
   },
