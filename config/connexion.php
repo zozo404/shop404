39b25b3d7c310afaab3abef8242ff1277f7be519
zozo404 <enzocuny66@gmail.com>
2022-11-18 10:29:57 +0100

    🎨 | format price

--- config/connexion.php
+++ config/connexion.php
@@ -2,8 +2,9 @@
 // connexion à la bdd
 $servername='localhost';
 $username = 'root';
+$database = 'shop404';
 try{
-    $access = new PDO("mysql:host=$servername;dbname=shop404;port=3306;charset=utf8", $username,"");
+    $access = new PDO("mysql:host=$servername;dbname=$database;port=3306;charset=utf8", $username,"");
     $access->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
 catch(PDOException $e){
--- user/index.php
+++ user/index.php
@@ -65,7 +65,7 @@ if (!isset($_SESSION['user001']) || empty($_SESSION['user001'])) {
                         <div class="flex space-x-2 justify-center">
                             <button type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Ajouter au panier</button>
                         </div>
-                        <bold><?= $produit->prix ?> €</bold>
+                        <bold><?=number_format($produit->prix, 2, ',', ' ') ?> €</bold>
                     </a>
                 </div>
             </div>
